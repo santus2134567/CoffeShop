e017c9f92175377cc5d0d9da4a2301c38acee360
ProgramacionMovil app01 <201913176e@utea.edu.pe>
2022-12-02 23:23:30 -0500

    principio-segregacion-interfaces

---
+++ principio-segregacion-interfaces/ducks.php
@@ -0,0 +1,59 @@
+<?php
+
+interface CanPlay {
+    public function play();
+}
+
+interface CanFloat {
+    public function float();
+}
+
+interface CanQuack {
+    public function quack();
+}
+
+class Duck implements CanFloat, CanQuack, CanPlay {
+    public function float() {
+        echo "The duck is floating\n";
+    }
+
+    public function quack() {
+        echo "The duck is quacking\n";
+    }
+
+    public function play() {
+        $this->float();
+        $this->quack();
+    }
+}
+
+class WoodenDuck implements CanFloat, CanPlay {
+
+    public function float() {
+        echo "The wooden is floating\n";
+    }
+
+    public function play() {
+        $this->float();
+    }
+
+}
+
+class Frog implements CanPlay {
+    public function play() {
+        echo "The frog is playing\n";
+    }
+}
+
+class Pond {
+    public function sendToPlay(CanPlay $player) {
+        $player->play();
+
+
+    }
+}
+
+$pond = new Pond();
+$pond->sendToPlay(new Duck());
+$pond->sendToPlay(new WoodenDuck());
+$pond->sendToPlay(new Frog());
